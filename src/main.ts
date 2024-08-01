eb024c463e02885597926958aa9240f93e00eae9
Sileniz <walissonmacedo02@gmail.com>
2024-08-01 16:56:27 -0300

    starting new feature

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import puppeteer, { type Browser, type Page } from 'puppeteer';
 import getTextsFromSelector from '../utils/getText';
-
+import getLinkFromSelector from "../utils/getLinkFromSelector"
 const mainFunction = async () => {
   const browser: Browser = await puppeteer.launch()
   const page: Page = await browser.newPage()
@@ -11,7 +11,7 @@ const mainFunction = async () => {
 
   const titles: Array<string | null> = await getTextsFromSelector(page, '.UpdatedTitle-module_titleName_1QO_s')
   const chapters: Array<string | null> = await getTextsFromSelector(page, '.UpdatedTitle-module_chapterTitle_kZUrz')
-
+  const chapter = await getLinkFromSelector(page, 'UpdatedTitle-module_titleWrapper_2EQIT')
   if((!titles || titles == null) || (!chapters || chapters == null)) {
     console.log("Houve um erro ao buscar as atualizações")
     await browser.close()
@@ -21,6 +21,7 @@ const mainFunction = async () => {
     console.log(`${titles[i]}: ${chapters[i] == 'ex' ? "Extra" : chapters[i]}`)
   }
   console.log('\n')
+  console.log(chapter)
   await browser.close()
 }
 
---
+++ utils/getLinkFromSelector.ts
@@ -0,0 +1,9 @@
+import type { Page } from "puppeteer";
+
+async function getLinkFromSelector(page: Page, selector: string): Promise<Array<string | null>> {
+  return page.evaluate(selector => {
+    const nodes = document.querySelectorAll(`div.${selector} > a`);
+    return Array.from(nodes).map(node => node.href);
+  }, selector);
+}
+export default getLinkFromSelector
